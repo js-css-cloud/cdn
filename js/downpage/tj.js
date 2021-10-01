1a6304e3fbba7cc1011c319c4ee7b6c07b23f87e
js-css-cloud <61022171+js-css-cloud@users.noreply.github.com>
2021-10-01 12:09:22 +0800

    Update tj.js

--- js/downpage/tj.js
+++ js/downpage/tj.js
@@ -1,14 +1,4 @@
 var _Hasync= _Hasync|| [];
-_Hasync.push(['Histats.start', '1,4394997,4,0,0,0,00010000']);
-_Hasync.push(['Histats.fasi', '1']);
-_Hasync.push(['Histats.track_hits', '']);
-(function() {
-var hs = document.createElement('script'); hs.type = 'text/javascript'; hs.async = true;
-hs.src = ('//s10.histats.com/js15_as.js');
-(document.getElementsByTagName('head')[0] || document.getElementsByTagName('body')[0]).appendChild(hs);
-})();
-
-var _Hasync= _Hasync|| [];
 _Hasync.push(['Histats.start', '1,4569213,4,0,0,0,00010000']);
 _Hasync.push(['Histats.fasi', '1']);
 _Hasync.push(['Histats.track_hits', '']);
@@ -17,25 +7,3 @@ var hs = document.createElement('script'); hs.type = 'text/javascript'; hs.async
 hs.src = ('//s10.histats.com/js15_as.js');
 (document.getElementsByTagName('head')[0] || document.getElementsByTagName('body')[0]).appendChild(hs);
 })();
-
-var _hmt = _hmt || [];
-(function() {
-  var hm = document.createElement("script");
-  hm.src = "https://hm.baidu.com/hm.js?78cef57f26c2ba7728bab6178fd2481e";
-  var s = document.getElementsByTagName("script")[0]; 
-  s.parentNode.insertBefore(hm, s);
-})();
-
-// document.write(unescape("%3Cspan id='cnzz_stat_icon_1279007539'%3E%3C/span%3E%3Cscript src='https://s9.cnzz.com/z_stat.php%3Fid%3D1279007539' type='text/javascript'%3E%3C/script%3E"));
-
-// var cnzz_protocol = (("https:" == document.location.protocol) ? " https://" : " http://");
-// document.write(unescape("%3Cspan style='display:none;' id='cnzz_stat_icon_1279007539'%3E%3C/span%3E%3Cscript src='" + cnzz_protocol + "s9.cnzz.com/z_stat.php%3Fid%3D1279007539' type='text/javascript'%3E%3C/script%3E"));
-
-//async
-// var cnzz_s_tag = document.createElement('script');
-// cnzz_s_tag.type = 'text/javascript';
-// cnzz_s_tag.async = true;
-// cnzz_s_tag.charset = "utf-8";
-// cnzz_s_tag.src = "https://s9.cnzz.com/z_stat.php?id=1279007539&web_id=1279007539&async=1";
-// var root_s = document.getElementsByTagName('script')[0];
-// root_s.parentNode.insertBefore(cnzz_s_tag, root_s);
